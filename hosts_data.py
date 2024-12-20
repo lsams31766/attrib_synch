9c5b7283947907e0561e7edfbbd69ad821c3fd0f
lsams31766 <larry_samuels@hotmail.com>
2024-12-20 10:19:45 -0500

    Create hosts_data.py

---
+++ hosts_data.py
@@ -0,0 +1,10 @@
+'''
+Created on Jul 14, 2023
+
+@author: larry
+'''
+
+# hosts name, url,  ssl_or_not, DN and password
+hosts_data = {
+    'local1389':("ldap://localhost:1389",'cn=admin,dc=rahasak,dc=com'),
+}
